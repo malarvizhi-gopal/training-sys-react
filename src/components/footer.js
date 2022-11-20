ff867359411bfa1cc83257b61ba280d32fea88f2
MALAR@UNBORN <malarvizhigopal@unborn.co.in>
2022-11-20 13:50:19 +0530

    training-sys all sample boostrap without node_modules.

---
+++ src/components/footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Footer1 = () => {
+    return ( 
+    <footer className = "d-block" >
+        <p className = "text-center" > ©Copyright 2022 UNBORN SOLUTIONS</p> </footer>
+    );
+};
+export default Footer1;
